0b0ba4593b5260a3c6dca745d89d2f203228ace5
Priyanka-ashok <priyanka.ashok@quintype.com>
2019-03-25 19:30:19 +0530

    Calculator App

---
+++ Output.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Output extends Component {
+    render() {
+        let { output } = this.props;
+        return (
+            <div className="Output">
+                <input type="numbers" value={output} id="display" />
+            </div>
+        );
+    }
+}
+
+
+export default Output;
+
